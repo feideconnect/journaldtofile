8930f6594ed1c17b54bf78d011b8572e42d96241
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-01-06 15:48:04 +0100

    Reopen output file when receiving sighup

--- journaldtofile.go
+++ journaldtofile.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 import "os"
+import "os/signal"
+import "syscall"
 import "time"
 
 import "github.com/mheese/go-systemd/sdjournal"
 
-func process(recv chan sdjournal.JournalEntry) {
+func process(recv chan sdjournal.JournalEntry, rotate chan os.Signal) {
 	fmt.Printf("In process\n")
 	outputfile, err := os.OpenFile("logfile.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
@@ -16,18 +18,28 @@ func process(recv chan sdjournal.JournalEntry) {
 	defer outputfile.Close()
 
 	var line string
-	for entry := range recv {
-		line = entry["MESSAGE"].(string) + "\n"
-		outputfile.WriteString(line)
+	for {
+		select {
+		case entry := <- recv:
+			line = entry["MESSAGE"].(string) + "\n"
+			outputfile.WriteString(line)
+		case <-rotate:
+			outputfile.Close()
+			outputfile, err = os.OpenFile("logfile.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+			if err != nil {
+				//		fmt.Printf("Could not open file: %v\n", err)
+				panic(err)
+			}
+		}
 	}
 }
 
 func main() {
+	done := make(chan int, 1)
+	recv := make(chan sdjournal.JournalEntry)
+	rotate := make(chan os.Signal)
 
-	var done chan int
-	var recv chan sdjournal.JournalEntry
-	done = make(chan int, 1)
-	recv = make(chan sdjournal.JournalEntry)
+	signal.Notify(rotate, syscall.SIGHUP)
 
 	jr, err := sdjournal.NewJournalReader(sdjournal.JournalReaderConfig{
 		Since: time.Duration(1),
@@ -37,7 +49,7 @@ func main() {
 		fmt.Printf("Could not create JournalReader: %v\n", err)
 		return
 	}
-	go process(recv)
+	go process(recv, rotate)
 
 	fmt.Printf("Starting followjournal\n")
 	jr.FollowJournal(done, recv)
